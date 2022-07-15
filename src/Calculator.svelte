820ac7b6b4500259c2f1a6285491fcee38be80e6
Caleb Hite <chite2457@gmail.com>
2022-07-14 19:55:45 -0500

    Commit message

--- src/Calculator.svelte
+++ src/Calculator.svelte
@@ -52,7 +52,7 @@
     #output{
         position: absolute;
         top: 155px;
-        right: -250px;
+        right: -240px;
         width: 1200px;
         height: 270px;
         border-radius: 5px;
